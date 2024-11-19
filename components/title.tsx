d332c1b243e589ba5380a5dd607febfc5a0ab953
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-19 10:31:23 +0530

    Enhance Title component to add pulse animation for 'New' indicator

--- components/title.tsx
+++ components/title.tsx
@@ -7,7 +7,7 @@ export const Title = ({ isNew, title }: Props) => {
     <div className="flex items-center justify-between w-full">
       <h1 className="text-sm">{title}</h1>
       {isNew && (
-        <span className="text-xs dark:text-green-400 text-green-700 border dark:border-green-400 border-green-700 px-2 py-1 rounded-lg">
+        <span className="text-xs dark:text-green-400 text-green-700 animate-pulse">
           New
         </span>
       )}
