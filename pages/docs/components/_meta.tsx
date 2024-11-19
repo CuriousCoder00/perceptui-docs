f886817bfacd19a5ce1a97645404b681062e631c
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-19 10:37:14 +0530

    Add Badge component documentation and update meta configuration

--- pages/docs/components/_meta.tsx
+++ pages/docs/components/_meta.tsx
@@ -2,5 +2,6 @@ import { Title } from "@/components/title";
 
 export default {
   avatar: { title: <Title isNew title="Avatar" /> },
+  badge: { title: <Title isNew title="Badge" /> },
   button: { title: <Title isNew title="Button" /> },
 };
