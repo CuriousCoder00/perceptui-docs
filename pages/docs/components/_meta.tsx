21ffdf9ed503909d5f7ca38754b8c4ad23bca046
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-21 01:08:25 +0530

    Add Alert component documentation and demo, update Badge and Button documentation

--- pages/docs/components/_meta.tsx
+++ pages/docs/components/_meta.tsx
@@ -1,6 +1,7 @@
 import { Title } from "@/components/title";
 
 export default {
+  alert: { title: <Title title="Alert" /> },
   avatar: { title: <Title title="Avatar" /> },
   badge: { title: <Title title="Badge" /> },
   button: { title: <Title title="Button" /> },
