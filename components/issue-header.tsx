199323e7ef3d6718c22f2f034ddddb1850e19886
Kapil Jangid <kapiljangid040@gmail.com>
2024-12-29 18:34:35 +0530

    Update IssueHeader component to use Link from Next.js; upgrade @perceptui/ui dependency to 0.0.16

--- components/issue-header.tsx
+++ components/issue-header.tsx
@@ -1,18 +1,19 @@
+import Link from "next/link";
 import React from "react";
 import { MdArrowOutward } from "react-icons/md";
 
 export const IssueHeader = ({ source }: { source: string }) => {
   return (
     <div className="flex justify-start items-center gap-7 my-3 p-2">
-      <a
+      <Link
         className="group text-sky-600 transition duration-300"
         href={`https://github.com/perceptui/ui/blob/main/apps/lib/src/${source}.tsx`}
         target="_blank"
       >
         View source <MdArrowOutward className="inline" />
         <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
-      </a>
-      <a
+      </Link>
+      <Link
         className="group text-sky-600 transition duration-300"
         href={`https://github.com/perceptui/ui/issues/new?title=[${
           source.split("/")[source.split("/").length - 1]
@@ -21,7 +22,7 @@ export const IssueHeader = ({ source }: { source: string }) => {
       >
         Report an issue <MdArrowOutward className="inline" />
         <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
-      </a>
+      </Link>
     </div>
   );
 };
